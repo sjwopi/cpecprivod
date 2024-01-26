4840781b90d25e6cc29adcff88518283bdff9847
Ivan Teteruk <iteterk@gmail.com>
2024-01-26 15:30:38 +0300

    feat modal form

---
+++ login.js
@@ -0,0 +1,39 @@
+const modalLogin = document.querySelector('.modalLogin');
+const modalRegister = document.querySelector('.modalRegister');
+const modalRegisterUr = document.querySelector('.modalRegisterUr');
+
+/* переключение между физ и юр лицом */
+modalRegister.querySelector('.btnToUr').addEventListener('click', (e) => {
+  e.preventDefault();
+  modalRegister.classList.remove('active');
+  modalRegisterUr.classList.add('active');
+});
+modalRegisterUr.querySelector('.btnToFiz').addEventListener('click', (e) => {
+  e.preventDefault();
+  modalRegister.classList.add('active');
+  modalRegisterUr.classList.remove('active');
+});
+
+/* переключение между войти и зарегестрироваться */
+modalLogin.querySelector('.btnChangeGo').addEventListener('click', (e) => {
+  e.preventDefault();
+  modalLogin.classList.remove('active');
+  modalRegister.classList.add('active');
+})
+modalRegister.querySelector('.btnChangeGo').addEventListener('click', (e) => {
+  e.preventDefault();
+  modalLogin.classList.add('active');
+  modalRegister.classList.remove('active');
+})
+
+document.querySelectorAll('.btnGo').forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href = "./profile.html";
+  })
+})
+
+document.querySelector(".head-profile").addEventListener('click', () => {
+  modalLogin.classList.add('active');
+})
+
